fc14431275666eacc2bc1b9bc36396ab32e0341d
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2021-03-17 21:11:57 -0400

    Last venture down 2nd dead end

--- src/Pusher.elm
+++ src/Pusher.elm
@@ -1,4 +1,4 @@
-module Pusher exposing (Member, decodeMember)
+module Pusher exposing (Member, EncodedMember, MemberList, EncodedMemberList, decodeMember, decodeMemberList)
 
 {-| An Elm interface to [Pusher Channels](https://pusher.com/channels)
 
@@ -9,11 +9,11 @@ Pusher channels can transport arbitrary JSON data, so our Pusher datatypes will 
 
 When you see `Value` below, please mentally substitute `Json.Decode.Value`. Your author doesn't know how to make `Json.Decode.Value` show up fully specified in the documentation.
 
-@docs Member, decodeMember
+@docs Member, EncodedMember, MemberList, EncodedMemberList, decodeMember, decodeMemberList
 
 -}
 
-import Json.Decode exposing (Decoder, Error, Value, decodeValue)
+import Json.Decode as Decode exposing (Decoder, Error(..), Value, decodeValue)
 
 
 {-| Pusher's `Member` objects have an `id` field which is a `String` and an `info` field that can hold arbitrary data. Your application will send that `info` data on login, and receive it again as membership information.
@@ -22,6 +22,29 @@ type alias Member infoType =
     { id : String
     , info : infoType
     }
+
+
+{-| An `EncodedMember` is a `Member Json.Decode.Value`.
+
+When we send a `Member` object to, or receive a `Member` object from, the Pusher server, the object's `info` field can be any JSON-representable type. We represent that in Elm as a record with a `Json.Decode.Value` in its `info` field.
+
+-}
+type alias EncodedMember =
+    Member Decode.Value
+
+
+{-| When you subscribe to a Pusher Presence channel, you'll get back a `MemberList` — which is not a simple `List Member` but a record with fields `me` (the current logged in user), and `members` (a list of all users, which is indeed a `List Member`).
+-}
+type alias MemberList infoType =
+    { me : Member infoType
+    , members : List (Member infoType)
+    }
+
+
+{-| An `EncodedMemberList` is a `MemberList Json.Decode.Value`
+-}
+type alias EncodedMemberList =
+    MemberList Decode.Value
 
 
 {-| The `decodeMember` function takes a JSON decoder for some `infoType` and a `Member Json.Decode.Value` and returns a `Result Json.Decoder.Error (Member infoType)`.
@@ -47,3 +70,65 @@ decodeMember : Decoder infoType -> Member Value -> Result Error (Member infoType
 decodeMember infoDecoder member =
     decodeValue infoDecoder member.info
         |> Result.map (\decoded -> { id = member.id, info = decoded })
+
+
+memberDecoder : Decoder infoType -> Decoder (Member infoType)
+memberDecoder decoder =
+    Decode.map2 Member
+        (Decode.field "id" Decode.string)
+        (Decode.field "info" decoder)
+
+
+{-| The `decodeMemberList` function takes a JSON decoder for some `infoType` and a `MemberList Json.Decode.Value` and returns a `Result Json.Decoder.Error (MemberList infoType)`.
+
+For instance:
+
+    import Json.Decode as Decode
+    import Json.Encode as Encode
+
+    let
+        encodeMember: Int -> Member Decode.Value
+        encodeMember n =
+            { id = "#" ++ (String.fromInt n), info = Encode.int n }
+        encodedMemberList = -- as it might come from Pusher
+            { me = encodeMember 1
+            , members = [encodeMember 0, encodeMember 1, encodeMember 2]
+            }
+        infoDecoder = Decode.int
+    in
+    decodeMemberList infoDecoder encodedMemberList
+
+    --> Ok { me = { id = "#1", info = 1 }
+    -->    , members =
+    -->       [ { id = "#0", info = 0 }
+    -->       , { id = "#1", info = 1 }
+    -->       , { id = "#2", info = 2 }
+    -->       ]
+    -->    }
+
+-}
+decodeMemberList : Decoder infoType -> MemberList Value -> Result Error (MemberList infoType)
+decodeMemberList infoDecoder memberList =
+    let
+        decode =
+            decodeMember infoDecoder
+
+        maybeMe =
+            Result.mapError (\error -> Field "me" error) (decode memberList.me)
+
+        memberAt n codedMember =
+            Result.mapError (\error -> Field "members" (Index n error)) (decode codedMember)
+
+        maybeMembers =
+            List.indexedMap memberAt memberList.members
+                |> List.foldr (Result.map2 (::)) (Ok [])
+    in
+    case ( maybeMe, maybeMembers ) of
+        ( Ok me, Ok members ) ->
+            Ok { me = me, members = members }
+
+        ( Err badMe, _ ) ->
+            Err badMe
+
+        ( _, Err badMember ) ->
+            Err badMember
--- tests/TestPusher.elm
+++ tests/TestPusher.elm
@@ -4,7 +4,7 @@ import Expect
 import Json.Decode as D
 import Json.Decode.Pipeline exposing (required)
 import Json.Encode as E
-import Pusher exposing (Member, decodeMember)
+import Pusher exposing (Member, MemberList, decodeMember, decodeMemberList)
 import Test exposing (Test, describe, test)
 
 
@@ -12,9 +12,9 @@ type alias InfoType =
     { name : String }
 
 
-jane : Member InfoType
-jane =
-    { id = "xyz.abc", info = { name = "Jane" } }
+infoDecoder : D.Decoder InfoType
+infoDecoder =
+    D.succeed InfoType |> required "name" D.string
 
 
 encodeMember : Member InfoType -> Member D.Value
@@ -22,23 +22,66 @@ encodeMember member =
     { id = member.id, info = E.object [ ( "name", E.string member.info.name ) ] }
 
 
-infoDecoder : D.Decoder InfoType
-infoDecoder =
-    D.succeed InfoType |> required "name" D.string
+jane : Member InfoType
+jane =
+    { id = "xyz.abc", info = { name = "Jane" } }
+
+
+encodedJane =
+    encodeMember jane
+
+
+memberWithBadInfo =
+    { id = "abc.xyz", info = E.int 2 }
+
+
+memberList : MemberList InfoType
+memberList =
+    { me = jane
+    , members =
+        [ { id = "1", info = { name = "One" } }
+        , jane
+        , { id = "42", info = { name = "Life, the universe, and everything" } }
+        , { id = "3", info = { name = "Three" } }
+        ]
+    }
+
+
+encodedMemberList : MemberList D.Value
+encodedMemberList =
+    { me = encodedJane
+    , members = List.map encodeMember memberList.members
+    }
+
+
+badMeInMemberList : MemberList D.Value
+badMeInMemberList =
+    { encodedMemberList | me = memberWithBadInfo }
+
+
+badListMemberInMemberList : MemberList D.Value
+badListMemberInMemberList =
+    let
+        m =
+            encodedMemberList.members
+
+        oneBad =
+            List.concat [ List.take 2 m, [ memberWithBadInfo ], List.drop 2 m ]
+    in
+    { encodedMemberList | members = oneBad }
 
 
 suite : Test
 suite =
-    let
-        badInfo =
-            { id = "xzy.abc", info = E.int 2 }
-
-        encodedJane =
-            encodeMember jane
-    in
     describe "Pusher tests"
         [ test "decodeMember returns an error when info can't be parsed" <|
-            \_ -> decodeMember infoDecoder badInfo |> Expect.err
-        , test "decodeMember returns a Result with the encoded info when .info is an encoded InfoType" <|
+            \_ -> decodeMember infoDecoder memberWithBadInfo |> Expect.err
+        , test "decodeMember returns Ok with the decoded info when .info is an encoded InfoType" <|
             \_ -> decodeMember infoDecoder encodedJane |> Expect.equal (Ok jane)
+        , test "decodeMemberList returns Ok with all .info fields decoded when they are encoded correctly" <|
+            \_ -> decodeMemberList infoDecoder encodedMemberList |> Expect.equal (Ok memberList)
+        , test "decodeMemberList returns an error when the `me` field can't be parsed" <|
+            \_ -> decodeMemberList infoDecoder badMeInMemberList |> Expect.err
+        , test "decodeMemberList returns an error when a `members` element can't be parsed" <|
+            \_ -> decodeMemberList infoDecoder badListMemberInMemberList |> Expect.err
         ]
