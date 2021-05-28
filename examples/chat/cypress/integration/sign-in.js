dd58aefabc5878503ad0be2ff0f4cd4b8378b9e5
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2021-05-27 21:23:26 -0400

    Add password; trim and validate name and password

--- examples/chat/cypress/integration/sign-in.js
+++ examples/chat/cypress/integration/sign-in.js
@@ -1,9 +1,23 @@
 describe('Sign-in tests', () => {
-  it('Gets the login page on first access', () => {
+  it('Redirects to main page only after name and password are entered', () => {
     cy.visit('http://localhost:8888/')
     cy.location('pathname').should('eq', '/sign-in')
-    cy.get('input[id="Name"]').type('Yarrow')
-    cy.get('button').click()
+
+    cy.get('[data-cy=name]').clear()
+    cy.location('pathname').should('eq', '/sign-in')
+
+    cy.get('[data-cy=sign-in]').click()
+    cy.get('[data-cy=error]').contains(/name/i)
+    cy.location('pathname').should('eq', '/sign-in')
+
+    cy.get('[data-cy=name]').type('     Yarrow        ') // Will be trimmed to 'Yarrow'
+    cy.get('[data-cy=password]').clear()
+    cy.get('[data-cy=sign-in]').click()
+    cy.get('[data-cy=error]').contains(/password/i)
+    cy.location('pathname').should('eq', '/sign-in')
+
+    cy.get('[data-cy=password]').type('any-damn-password')
+    cy.get('[data-cy=sign-in]').click()
     cy.location('pathname').should('eq', '/')
     cy.contains('Yarrow')
   })
--- examples/chat/src/Pages/SignIn.elm
+++ examples/chat/src/Pages/SignIn.elm
@@ -2,12 +2,13 @@ module Pages.SignIn exposing (Model, Msg, init, page, update, view)
 
 import Effect exposing (Effect)
 import Gen.Params.SignIn exposing (Params)
-import Html.Styled as Html exposing (button, div, input, label, p, text)
-import Html.Styled.Attributes exposing (for, id, placeholder, value)
+import Html.Styled as Html exposing (Attribute, Html, br, button, div, input, label, p, text)
+import Html.Styled.Attributes exposing (attribute, for, id, placeholder, type_, value)
 import Html.Styled.Events as Events
 import Page
 import Request
 import Shared
+import Validate exposing (Validator, validate)
 import View exposing (View)
 
 
@@ -27,12 +28,51 @@ page shared _ =
 
 type alias Model =
     { name : String
+    , password : String
+    , error : Maybe String
     }
 
 
 init : Shared.Model -> ( Model, Effect Msg )
 init shared =
-    ( { name = Maybe.withDefault { name = "" } shared.user |> .name }, Effect.none )
+    ( { name = Maybe.withDefault { name = "" } shared.user |> .name
+      , password = ""
+      , error = Nothing
+      }
+    , Effect.none
+    )
+
+
+
+-- VALIDATION
+
+
+modelValidator : Validator String Model
+modelValidator =
+    Validate.firstError
+        [ Validate.ifBlank .name "Please type your name"
+        , Validate.ifBlank .password "Please enter the password"
+        ]
+
+
+trimAndValidate : Model -> Result String Model
+trimAndValidate model =
+    let
+        trimmedModel =
+            { model
+                | name = String.trim model.name
+                , password = String.trim model.password
+            }
+
+        result =
+            validate modelValidator trimmedModel
+    in
+    case result of
+        Ok trimmed ->
+            Ok (Validate.fromValid trimmed)
+
+        Err errors ->
+            Err (String.join "; " errors)
 
 
 
@@ -41,19 +81,30 @@ init shared =
 
 type Msg
     = ClickedSignIn
-    | UpdateName String
+    | NameChanged String
+    | PasswordChanged String
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
     case msg of
         ClickedSignIn ->
-            ( model
-            , Effect.fromShared (Shared.SignIn { name = model.name })
-            )
+            case trimAndValidate model of
+                Ok validatedModel ->
+                    ( { validatedModel | error = Nothing }
+                    , Effect.fromShared (Shared.SignIn { name = model.name })
+                    )
 
-        UpdateName name ->
+                Err error ->
+                    ( { model | error = Just error }
+                    , Effect.none
+                    )
+
+        NameChanged name ->
             ( { model | name = name }, Effect.none )
+
+        PasswordChanged password ->
+            ( { model | password = password }, Effect.none )
 
 
 
@@ -69,24 +120,52 @@ subscriptions _ =
 -- VIEW
 
 
+errorView : Maybe String -> Html msg
+errorView err =
+    case err of
+        Nothing ->
+            div [] []
+
+        Just complaint ->
+            div [ testId "error" ] [ text complaint ]
+
+
+testId : String -> Attribute msg
+testId id =
+    attribute "data-cy" id
+
+
 view : Model -> View Msg
 view model =
     { title = "Sign In"
     , body =
         [ div []
             [ p []
-                [ label [ for "Name" ] [ text "Name: " ]
+                [ label [ for "name" ] [ text "Name: " ]
                 , input
                     [ placeholder "Your name"
                     , value model.name
-                    , Events.onInput UpdateName
-                    , id "Name"
+                    , Events.onInput NameChanged
+                    , id "name"
+                    , testId "name"
                     ]
                     []
+                , br [] []
+                , label [ for "password" ] [ text "Password: " ]
+                , input
+                    [ type_ "password"
+                    , placeholder "Password"
+                    , value model.password
+                    , Events.onInput PasswordChanged
+                    , id "password"
+                    , testId "password"
+                    ]
+                    []
+                , errorView model.error
                 ]
             ]
         , button
-            [ Events.onClick ClickedSignIn ]
+            [ testId "sign-in", Events.onClick ClickedSignIn ]
             [ text "Sign in" ]
         ]
     }
